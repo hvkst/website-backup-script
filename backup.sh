5d1ad20f387ab6991d6470843a36394ea501ed35
Bernd Havekost <bernd@hvkst.de>
2022-04-30 09:28:28 +0200

    added date/time/folders, remove the command to create a final zip, changed backup from zip to tar

--- backup.sh
+++ backup.sh
@@ -3,6 +3,9 @@
 #Alan Smith | smithalan2@gmail.com
 # 28/01/2016
 ###################
+#Bernd Havekost | bernd@hvkst.de
+# 30/04/2022
+###################
 #!/bin/sh
 ##You need to change the below values##
 #MySQL Username
@@ -17,28 +20,29 @@ mysql_db="db_name"
 file_dir="/var/www/html/mysite"
 #The folder to save the backups in
 backupfolder="/var/home/user/backups"
-##No need to change anything below here##
-#Get The current date/time
-now="$(date +'%d_%m_%Y_%H_%M_%S')"
+now="$(date +'%d_%m_%Y_%H_%M')"
+#Date and time for the new Folder
+date="$(date +'%d_%m_%Y')"
+time="$(date +'%H_%M')"
 #Set the backup filename with todays date/time
-filename="backup_$now".zip
-#Temp path for database dump
-temp_mysql="$backupfolder"/db.sql
-#Temp path for file dump
-temp_files="$backupfolder"/files.zip
-#Path to the final backup
-fullpathbackupfile="$backupfolder/$filename"
+filename="backup_$now".tar.gz
+#Path for database dump
+temp_mysql="$backupfolder"/"db_$now".sql
+#Path for file dump
+temp_files="$backupfolder"/"backup_$now".tar.gz
 #Start MYSQL Dump
 echo "Starting mysql dump"
 mysqldump --user=$mysql_user --password=$mysql_pass --host=$mysql_host $mysql_db > $temp_mysql
 echo "Finished mysql dump, starting file dump"
 #Zip all files in specified directory
-zip -r -q $temp_files $file_dir
-echo "File Dump Finished, Adding DB And Files to a single zip"
-#Add database dump and file dump to a zip
-zip -r -q $fullpathbackupfile $temp_mysql $temp_files
-echo "Done with zip, removing unneeded files"
-#Remove temp mysql file and temp files zip file
-rm $temp_mysql
-rm $temp_files
-echo "Backup finished. Your backup is located at "$fullpathbackupfile
+tar -cvzf $temp_files $file_dir
+echo "File Dump Finished."
+#Create Folders with date and time
+mkdir -p $backupfolder/$date/$time
+echo "Backup Folder created"
+#Move backups into date/time-Folder
+mv $backupfolder/$filename $backupfolder/$date/$time/
+mv "$backupfolder"/"db_$now".sql $backupfolder/$date/$time/
+echo "Backups moved to $date/$time"
+echo "Backup finished. Your backup is located at "$backupfolder/$date/$time/
+
